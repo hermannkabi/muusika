6afeb04c29c46d1cdcbadd3b2cf51621b9bb1e80
hermannkabi <hermann.kaebi@gmail.com>
2025-01-14 22:02:17 +0200

    Create your own playlists

---
+++ kohandatud/index.js
@@ -0,0 +1,826 @@
+var data = [
+    {
+      "title": "Prelüüd ja fuuga C-duur kogumikust HTK",
+      "artist": "J. S. Bach",
+      "id": "bach1",
+      "uid": "1"
+    },
+    {
+      "title": "Toccata d-moll",
+      "artist": "J. S. Bach",
+      "id": "bach2",
+      "uid": "2"
+    },
+    {
+      "title": "Brandenburgi kontsert nr. 3 G-duur I osa",
+      "artist": "J. S. Bach",
+      "id": "bach3",
+      "uid": "3"
+    },
+    {
+      "title": "Aaria orkestrisüidist nr. 3 D-duur",
+      "artist": "J. S. Bach",
+      "id": "bach4",
+      "uid": "4"
+    },
+    {
+      "title": "Matteuse passiooni lõpukoor",
+      "artist": "J. S. Bach",
+      "id": "bach5",
+      "uid": "5"
+    },
+    {
+      "title": "Kaanon",
+      "artist": "J. Pachelbel",
+      "id": "pachelbel",
+      "uid": "6"
+    },
+    {
+      "title": "Kevad I osa, soolokontsertide tsüklist „Aastaajad“",
+      "artist": "A. Vivaldi",
+      "id": "vivaldi1",
+      "uid": "7"
+    },
+    {
+      "title": "Talv I osa, soolokontsertide tsüklist „Aastaajad“",
+      "artist": "A. Vivaldi",
+      "id": "vivaldi2",
+      "uid": "8"
+    },
+    {
+      "title": "Xerxese largo ooperist „Xerxes“",
+      "artist": "G. Händel",
+      "id": "handel1",
+      "uid": "9"
+    },
+    {
+      "title": "Hallelujah koor oratooriumist „Messias“",
+      "artist": "G. Händel",
+      "id": "handel2",
+      "uid": "10"
+    },
+    {
+      "title": "Soprani aaria „Ma tean, et mu päästja elab“ oratooriumist „Messias“",
+      "artist": "G. Händel",
+      "id": "handel3",
+      "uid": "11"
+    },
+    {
+      "title": "Tulevärgimuusika. Avamäng",
+      "artist": "G. Händel",
+      "id": "handel4",
+      "uid": "12"
+    },
+    {
+      "title": "Üllatussümfoonia II osa (variatsioonid)",
+      "artist": "J. Haydn",
+      "id": "haydn1",
+      "uid": "13"
+    },
+    {
+      "title": "Keisrikvartett II osa (variatsioonid)",
+      "artist": "J. Haydn",
+      "id": "haydn2",
+      "uid": "14"
+    },
+    {
+      "title": "Trompetikontsert III osa (rondo)",
+      "artist": "J. Haydn",
+      "id": "haydn3",
+      "uid": "15"
+    },
+    {
+      "title": "40. sümfoonia I osa (sonaadivorm)",
+      "artist": "W. A. Mozart",
+      "id": "mozart1",
+      "uid": "16"
+    },
+    {
+      "title": "Klaverikontsert nr. 21 C-duur II osa",
+      "artist": "W. A. Mozart",
+      "id": "mozart2",
+      "uid": "17"
+    },
+    {
+      "title": "Väike öömuusika I osa",
+      "artist": "W. A. Mozart",
+      "id": "mozart3",
+      "uid": "18"
+    },
+    {
+      "title": "Requiem aeternam „Reekviemist“",
+      "artist": "W. A. Mozart",
+      "id": "mozart4",
+      "uid": "19"
+    },
+    {
+      "title": "Lacrimosa „Reekviemist“",
+      "artist": "W. A. Mozart",
+      "id": "mozart5",
+      "uid": "20"
+    },
+    {
+      "title": "Avamäng ooperile „Figaro pulm“",
+      "artist": "W. A. Mozart",
+      "id": "mozart6",
+      "uid": "21"
+    },
+    {
+      "title": "Figaro aaria ooperist „Figaro pulm“",
+      "artist": "W. A. Mozart",
+      "id": "mozart7",
+      "uid": "22"
+    },
+    {
+      "title": "Duett „Käekene mulle anna“ ooperist „Don Giovanni“",
+      "artist": "W. A. Mozart",
+      "id": "mozart8",
+      "uid": "23"
+    },
+    {
+      "title": "Öökuninganna aaria ooperist „Võluflööt“",
+      "artist": "W. A. Mozart",
+      "id": "mozart9",
+      "uid": "24"
+    },
+    {
+      "title": "Papageno aaria ooperist „Võluflööt“",
+      "artist": "W. A. Mozart",
+      "id": "mozart10",
+      "uid": "25"
+    },
+    {
+      "title": "9. sümfoonia 4. osa",
+      "artist": "L. van Beethoven",
+      "id": "beethoven1",
+      "uid": "26"
+    },
+    {
+      "title": "klaverisonaat „Appassionata“ 1. osa",
+      "artist": "L. van Beethoven",
+      "id": "beethoven2",
+      "uid": "27"
+    },
+    {
+      "title": "5. sümfoonia 1. osa",
+      "artist": "L. van Beethoven",
+      "id": "beethoven3",
+      "uid": "28"
+    },
+    {
+      "title": "„Kuupaistesonaat“ 1. osa",
+      "artist": "L. van Beethoven",
+      "id": "beethoven4",
+      "uid": "29"
+    },
+    {
+      "title": "Polonees A-duur",
+      "artist": "F. Chopin",
+      "id": "chopin1",
+      "uid": "30"
+    },
+    {
+      "title": "Etüüd c-moll op.10 nr. 12 (Revolutsiooniline)",
+      "artist": "F. Chopin",
+      "id": "chopin2",
+      "uid": "31"
+    },
+    {
+      "title": "Vihmapiiskade prelüüd",
+      "artist": "F. Chopin",
+      "id": "chopin3",
+      "uid": "32"
+    },
+    {
+      "title": "Nokturn Es-duur op.9 nr.2",
+      "artist": "F. Chopin",
+      "id": "chopin4",
+      "uid": "33"
+    },
+    {
+      "title": "Nokturn \"Armuunelmad\"",
+      "artist": "F. Liszt",
+      "id": "liszt1",
+      "uid": "34"
+    },
+    {
+      "title": "Ungari rapsoodia",
+      "artist": "F. Liszt",
+      "id": "liszt2",
+      "uid": "35"
+    },
+    {
+      "title": "Etüüd Metsik jaht kogumikust Transendentsed etüüdid klaverile",
+      "artist": "F. Liszt",
+      "id": "liszt3",
+      "uid": "36"
+    },
+    {
+      "title": "Hommikumeeleolu süidist Peer Gynt",
+      "artist": "E.Grieg",
+      "id": "grieg1",
+      "uid": "37"
+    },
+    {
+      "title": "\"Klaverikontsert\" 1. osa",
+      "artist": "E.Grieg",
+      "id": "grieg2",
+      "uid": "38"
+    },
+    {
+      "title": "Intermezzo ooperist \"Talupoja au\"",
+      "artist": "P. Mascagni",
+      "id": "mascagni",
+      "uid": "39"
+    },
+    {
+      "title": "\"Lõpetamata sümfoonia\" 1. osa",
+      "artist": "F. Schubert",
+      "id": "schubert1",
+      "uid": "40"
+    },
+    {
+      "title": "sümfooniline poeem \"Finlandia\"",
+      "artist": "J. Sibelius",
+      "id": "sibelius1",
+      "uid": "41"
+    },
+    {
+      "title": "\"6. sümfoonia\" 1. osa",
+      "artist": "P. Tšaikovski",
+      "id": "tsaikovski1",
+      "uid": "42"
+    },
+    {
+      "title": "\"1. klaverikontsert\" 1. osa",
+      "artist": "P. Tšaikovski",
+      "id": "tsaikovski2",
+      "uid": "43"
+    },
+    {
+      "title": "\"Lillede valss\" balletist \"Pähklipureja\"",
+      "artist": "P. Tšaikovski",
+      "id": "tsaikovski3",
+      "uid": "44"
+    },
+    {
+      "title": "Avamäng ooperile Tannhäuser",
+      "artist": "R. Wagner",
+      "id": "wagner",
+      "uid": "45"
+    },
+    {
+      "title": "Sümfooniline poeem Prelüüdid",
+      "artist": "F. Liszt",
+      "id": "liszt4",
+      "uid": "46"
+    },
+    {
+      "title": "Klaverikontsert A-moll I osa",
+      "artist": "F. Schumann",
+      "id": "schumann",
+      "uid": "47"
+    },
+    {
+      "title": "3. sümfoonia 3. osa ",
+      "artist": "J. Brahms",
+      "id": "brahms1",
+      "uid": "48"
+    },
+    {
+      "title": "Ungari tants nr 5",
+      "artist": "J. Brahms",
+      "id": "brahms2",
+      "uid": "49"
+    },
+    {
+      "title": "Soololaul \"Metshaldjas\"",
+      "artist": "F. Schubert",
+      "id": "schubert2",
+      "uid": "50"
+    },
+    {
+      "title": "Kroonimisstseen ooperist \"Boris Godunov\"",
+      "artist": "M. Mussorgski",
+      "id": "mussorgski",
+      "uid": "51"
+    },
+    {
+      "title": "Habanera ooperist \"Carmen\"",
+      "artist": "G. Bizet",
+      "id": "bizet1",
+      "uid": "52"
+    },
+    {
+      "title": "Don Jose aaria ooperist Carmen ",
+      "artist": "G. Bizet",
+      "id": "bizet2",
+      "uid": "53"
+    },
+    {
+      "title": "Hertsogi lauluke ooperist \"Rigoletto\"",
+      "artist": "G. Verdi",
+      "id": "verdi1",
+      "uid": "54"
+    },
+    {
+      "title": "Joogilaul ooperist \"Traviata\"",
+      "artist": "G. Verdi",
+      "id": "verdi2",
+      "uid": "55"
+    },
+    {
+      "title": "Orjade koor ooperist \"Nabucco\"",
+      "artist": "G. Verdi",
+      "id": "verdi3",
+      "uid": "56"
+    },
+    {
+      "title": "Gospel - Down By The Riverside",
+      "artist": "Mahalia Jackson",
+      "id": "down-by-the-riverside",
+      "uid": "57"
+    },
+    {
+      "title": "Blues - Nobody Knows You When You're Down And Out",
+      "artist": "Bessie Smith",
+      "id": "nobody-knows",
+      "uid": "58"
+    },
+    {
+      "title": "Ragtime - The Entertainer",
+      "artist": "Scott Joplin",
+      "id": "entertainer",
+      "uid": "59"
+    },
+    {
+      "title": "Boogie woogie",
+      "artist": "Boogie woogie",
+      "id": "boogie-woogie",
+      "uid": "60"
+    },
+    {
+      "title": "New Orleans'i jazz - Dippermouth Blues",
+      "artist": "King Oliver's Creole Jazz Band",
+      "id": "dippermouth",
+      "uid": "61"
+    },
+    {
+      "title": "New Orleans'i jazz - What a Wonderful World",
+      "artist": "Louis Armstrong",
+      "id": "what-a-wonderful-world",
+      "uid": "62"
+    },
+    {
+      "title": "Swing - Sing Sing Sing - from 'Hollywood Hotel'",
+      "artist": "Benny Goodman orchestra",
+      "id": "sing-sing-sing",
+      "uid": "63"
+    },
+    {
+      "title": "Swing - In The Mood",
+      "artist": "Glenn Miller",
+      "id": "in-the-mood",
+      "uid": "64"
+    },
+    {
+      "title": "Swing - It don`t Mean a Thing",
+      "artist": "Duke Ellington",
+      "id": "it-dont-mean-a-thing",
+      "uid": "65"
+    },
+    {
+      "title": "Scat-laul - One Note Samba",
+      "artist": "Ella Fitzgerald",
+      "id": "one-note-samba",
+      "uid": "66"
+    },
+    {
+      "title": "Bebop",
+      "artist": "Charlie Parker",
+      "id": "bebop",
+      "uid": "67"
+    },
+    {
+      "title": "Bebop - A Night in Tunisia",
+      "artist": "Dizzy Gillespie",
+      "id": "a-night-in-tunisia",
+      "uid": "68"
+    },
+    {
+      "title": "Cool jazz - Blues in Green",
+      "artist": "Miles Davis",
+      "id": "blues-in-green",
+      "uid": "69"
+    },
+    {
+      "title": "Free jazz",
+      "artist": "Ornette Coleman Sextet",
+      "id": "free-jazz",
+      "uid": "70"
+    },
+    {
+      "title": "Jazz rock (fusion) - Burn",
+      "artist": "Miles Davis (ja tema ansambel)",
+      "id": "burn",
+      "uid": "71"
+    },
+    {
+      "title": "Jazz rock (fusion) - Birdland",
+      "artist": "Weather Report",
+      "id": "birdland",
+      "uid": "72"
+    },
+    {
+      "title": "Jazz rock (fusion) - Cantelope Island",
+      "artist": "Herbie Hancock (ja tema ansambel)",
+      "id": "cantelope-island",
+      "uid": "73"
+    },
+    {
+      "title": "World jazz - Bossa Nova",
+      "artist": "Latin jazz",
+      "id": "bossa-nova",
+      "uid": "74"
+    },
+    {
+      "title": "Acid jazz - Virtual Insanity",
+      "artist": "Jamiroquai",
+      "id": "virtual-insanity",
+      "uid": "75"
+    },
+    {
+      "title": "IV sümfoonia III osa",
+      "artist": "G. Mahler",
+      "id": "mahler-4",
+      "uid": "76"
+    },
+    {
+      "title": "Rhapsody in Blue",
+      "artist": "G. Gershwin",
+      "id": "gershwin",
+      "uid": "77"
+    },
+    {
+      "title": "Montecchid ja Capulettid balletist 'Romeo ja Julia'",
+      "artist": "S. Prokofjev",
+      "id": "prokofjev",
+      "uid": "78"
+    },
+    {
+      "title": "Bolero",
+      "artist": "M. Ravel",
+      "id": "ravel",
+      "uid": "79"
+    },
+    {
+      "title": "Fauni pärastlõuna",
+      "artist": "C. Debussy",
+      "id": "debussy",
+      "uid": "80"
+    },
+    {
+      "title": "Oo, Fortuna",
+      "artist": "C. Orff",
+      "id": "orff",
+      "uid": "81"
+    },
+    {
+      "title": "Heavy Smoke - City Life 5. osa",
+      "artist": "S. Reich",
+      "id": "reich",
+      "uid": "82"
+    },
+    {
+      "title": "Koyaanisqatsi",
+      "artist": "P. Glass",
+      "id": "koyaanisqatsi",
+      "uid": "83"
+    },
+    {
+      "title": "Ellujääja Varssavist",
+      "artist": "A. Schönberg",
+      "id": "schonberg",
+      "uid": "84"
+    },
+    {
+      "title": "Kontsert prepareeritud klaverile",
+      "artist": "J. Cage",
+      "id": "cage",
+      "uid": "85"
+    },
+    {
+      "title": "Kevadpühitsus",
+      "artist": "I. Stravinski",
+      "id": "stravinski",
+      "uid": "86"
+    },
+    {
+      "title": "VII sümfoonia I osa",
+      "artist": "D. Šostakovitš",
+      "id": "sostakovits",
+      "uid": "87"
+    },
+    {
+      "title": "II klaverikontserdi I osa",
+      "artist": "S. Rahmaninov",
+      "id": "rahmaninov",
+      "uid": "88"
+    },
+    {
+      "title": "II klaverikontserdi II osa",
+      "artist": "S. Rahmaninov",
+      "id": "rahmaninov2",
+      "uid": "89"
+    },
+    {
+      "title": "Sind surmani",
+      "artist": "A. Kunileid",
+      "id": "sind-surmani",
+      "uid": "90"
+    },
+    {
+      "title": "Mu isamaa on minu arm",
+      "artist": "A. Kunileid",
+      "id": "mu-isamaa",
+      "uid": "91"
+    },
+    {
+      "title": "Meestelaul",
+      "artist": "M. Härma",
+      "id": "meestelaul",
+      "uid": "92"
+    },
+    {
+      "title": "Tuljak",
+      "artist": "M. Härma",
+      "id": "tuljak",
+      "uid": "93"
+    },
+    {
+      "title": "Sanctum - oratooriumist 'Joonase lähetamine'",
+      "artist": "R. Tobias",
+      "id": "sanctum",
+      "uid": "94"
+    },
+    {
+      "title": "Eks teie tea",
+      "artist": "R. Tobias",
+      "id": "eks-teie-tea",
+      "uid": "95"
+    },
+    {
+      "title": "Põhjavaim",
+      "artist": "M. Saar",
+      "id": "pohjavaim",
+      "uid": "96"
+    },
+    {
+      "title": "Leelo",
+      "artist": "M. Saar",
+      "id": "leelo",
+      "uid": "97"
+    },
+    {
+      "title": "Koit",
+      "artist": "M. Lüdig",
+      "id": "koit",
+      "uid": "98"
+    },
+    {
+      "title": "Jaani hobu",
+      "artist": "V. Tormis",
+      "id": "jaanihobu",
+      "uid": "99"
+    },
+    {
+      "title": "Oh laula ja hõiska",
+      "artist": "K.A. Hermann",
+      "id": "oh-laula",
+      "uid": "100"
+    },
+    {
+      "title": "Kauneimad laulud",
+      "artist": "F.A. Saebelmann",
+      "id": "kauneimad-laulud",
+      "uid": "101"
+    },
+    {
+      "title": "Taaveti laul nr 141",
+      "artist": "C. Kreek",
+      "id": "taavet-141",
+      "uid": "102"
+    },
+    {
+      "title": "Universumi hääled",
+      "artist": "U. Sisask",
+      "id": "universumi-haaled",
+      "uid": "103"
+    },
+    {
+      "title": "Sabatants - balletist 'Kratt'",
+      "artist": "E. Tubin",
+      "id": "sabatants",
+      "uid": "104"
+    },
+    {
+      "title": "V sümfoonia I osa",
+      "artist": "E. Tubin",
+      "id": "viies-sumfoonia",
+      "uid": "105"
+    },
+    {
+      "title": "II sümfoonia I osa",
+      "artist": "L. Sumera",
+      "id": "teine-sumfoonia",
+      "uid": "106"
+    },
+    {
+      "title": "Fratres",
+      "artist": "A. Pärt",
+      "id": "fratres",
+      "uid": "107"
+    },
+    {
+      "title": "Arhitektoonika nr 6",
+      "artist": "E.S. Tüür",
+      "id": "arhitektoonika",
+      "uid": "108"
+    },
+    {
+      "title": "Kodumaine viis",
+      "artist": "H. Eller",
+      "id": "kodumaine-viis",
+      "uid": "109"
+    }
+  ];
+
+  const urlParams = new URLSearchParams(window.location.search);
+
+  const code = urlParams.get("c");
+
+  if(code){
+      var decodedData = atob(code.replace(/-/g, '+').replace(/_/g, '/')).split(";");
+      $(".create-playlist-container").hide();
+      
+      $(".playlist-info").hide();
+
+      $(".title").text(decodedData[0]);
+      $(document).prop('title', decodedData[0] + " - Muusika kuulamiskavad");
+      
+      data = data.filter((e)=>decodedData.slice(1).includes(e.uid));
+  }
+
+for(var i = 0; i<data.length; i++){
+    var song = data[i];
+
+    $(".music-tiles").append(
+        !code ? `<div class='wrapper' style="position:relative;"><input song-uid=`+song.uid+` type='checkbox' />
+            <div class="music-tile pause" song-id="`+song.id+`">
+            <div class="info">
+                <p>`+song.title+`</p>
+                <p>`+song.artist+`</p>
+            </div>
+            <p class="play-btn"><i class='material-icons'>play_arrow</i></p>
+        </div></div>` : `
+            <div class="music-tile pause" song-id="`+song.id+`">
+            <div class="info">
+                <p>`+song.title+`</p>
+                <p>`+song.artist+`</p>
+            </div>
+            <p class="play-btn"><i class='material-icons'>play_arrow</i></p>
+        </div>`
+    );
+
+    $(".audios").append(
+     `<audio id="`+song.id+`">
+        <source src="../audio/`+song.id+`.mp3" type="audio/mpeg">
+        Sinu brauser ei toeta audiot.
+    </audio>`
+    );
+}
+
+$(".music-tile").click(function (){
+    const id = $(this).attr("song-id");
+
+    var audioElement = document.getElementById(id);
+    var playSymbol = "play_arrow";
+
+    var playButton = $(this).find(".play-btn");
+
+    // Paused, play
+    if($(this).hasClass("pause")){
+        $(".music-tile:not(.pause)").click();
+        $(this).removeClass("pause");
+        audioElement.play();
+        playSymbol = "pause";
+    }else{
+        // Pause playing
+        $(this).addClass("pause");
+
+        if(audioElement != null) audioElement.pause();
+    }
+
+    $(this).find(".play-btn > .material-icons").text(playSymbol);
+});
+
+// Start the song from the start on double click
+$(".music-tile").dblclick(function (){
+    const id = $(this).attr("song-id");
+
+    var audioElement = document.getElementById(id);
+
+    audioElement.currentTime = 0;
+});
+
+$(".mode-choice").click(function (){
+
+    $(".mode-choice").removeClass("chosen");
+    $(this).addClass("chosen");
+
+    if($(this).hasClass("practice")){
+        $(".learn-screen").hide();
+        $(".practice-screen").show();
+    }else if($(this).hasClass("learn")){
+        $(".learn-screen").show();
+        $(".practice-screen").hide();
+    }
+});
+
+
+
+var currentSong;
+
+function chooseRandomSong(){
+    return data[Math.floor(Math.random()*data.length)];
+}
+
+
+function changeSong(){
+    $("#correct-hint").hide();
+    var song = chooseRandomSong();
+
+    currentSong = song;
+
+    var audio = document.getElementById(song.id);
+
+    audio.currentTime = 0;
+    $("#correct-name").text(song.title);
+    $("#correct-artist").text(song.artist);
+
+
+    audio.play();
+
+    $(".test-toggle-song > .material-icons").text("pause");
+}
+
+$(".start-test").click(function (){
+    $(this).hide();
+
+    // Choose a random song
+    changeSong();
+
+
+    $(".game-screen").show();
+});
+
+$(".test-toggle-song").click(function (){
+    var audio = document.getElementById(currentSong.id);
+    var buttonText = "⏸";
+
+    if($(this).hasClass("pause")){
+        $(this).removeClass("pause");
+        audio.play();
+        buttonText = "pause";
+    }else{
+        $(this).addClass("pause");
+        audio.pause();
+        buttonText = "play_arrow";
+    }
+
+    $(".test-toggle-song > .material-icons").text(buttonText);
+});
+
+$(".next-song").click(function (){
+    $(".correct-answer").text("Õige vastus oli ");
+
+    $("#correct-was-name").text(currentSong.title);
+    $("#correct-was-artist").text(currentSong.artist);
+
+    var audio = document.getElementById(currentSong.id);
+    audio.pause();
+    audio.currentTime = 0;
+
+    changeSong();
+});
+
+$("#song-duration").on("input", function (){
+    const val = $(this).val();
+
+    var audio = document.getElementById(currentSong.id);
+    audio.currentTime = val;
+});
+
+$("#view-correct").click(function (){
+    $("#correct-hint").fadeToggle(200);
+});
